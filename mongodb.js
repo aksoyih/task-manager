a2ea56b03c036d7108b8261fa338d5986568c872
haluk <i.halukaksoy@gmail.com>
2021-07-04 21:05:50 +0300

    findOne and find exercises

--- mongodb.js
+++ mongodb.js
@@ -1,72 +1,36 @@
 //CRUD operations -> create, read, update, delete
+const {MongoClient, ObjectID } = require('mongodb')
 
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
-
-const connectionURl = "mongodb://127.0.0.1:27017";
-const databaseName = "task-manager";
+const connectionURl = "mongodb://127.0.0.1:27017"
+const databaseName = "task-manager"
 
 MongoClient.connect(
-  connectionURl, { useNewUrlParser: true, useUnifiedTopology: true },
-  (error, client) => {
-  if (error) {
-    return console.log(error);
-  }
-  
-    const db = client.db(databaseName);
+    connectionURl, { useNewUrlParser: true, useUnifiedTopology: true },
+    (error, client) => {
+        if (error) {
+          return console.log(error)
+        }
 
-    // db.collection("users").insertOne(
-    //   {
-    //     name: "Haluk",
-    //     age: 23,
-    //   },
-    //   (error, result) => {
-        // if (error) {
-        //   return console.log("Unable to insert user");
-        // }
+        const db = client.db(databaseName)
 
-        // console.log(result.ops);
-    //   }
-    // );
+        // db.collection('users').findOne({/*name: 'Donald'*/ _id: new ObjectID('60e1eff62764dd4f3ca4c11f')}, (error, user) => {
+        //     if(error){
+        //         return console.log(error)
+        //     }
 
-    // db.collection("users").insertMany([
-    //   {
-    //     name: "Jen",
-    //     age: 28,
-    //   },
-    //   {
-    //     name: "Donald",
-    //     age: 27,
-    //   },
-    // ], (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to insert user");
-    //     }
+        //     if(user === null){
+        //         return console.log('no document found')
+        //     }
 
-    //     console.log(result.ops);
-    // });
+        //     console.log(user)
+        // })
 
-      db.collection("tasks").insertMany([
-      {
-        description: "Task 1",
-        completed: true,
-      },
-      {
-        description: "Task 2",
-        completed: false,
-      },
-      {
-        description: "Task 3",
-        completed: true,
-      },
-      ], (error, result) => {
-          if (error) {
-            return console.log("Unable to insert tasks");
-          }
+        // db.collection('users').find({ age: 27}).toArray((error,users) => {
+        //     if(error){
+        //         return console.log(error)
+        //     }
 
-          console.log(result.ops);
-      });
-
-
-  }
-);
+        //     console.log(users)
+        // })
+    }
+)
