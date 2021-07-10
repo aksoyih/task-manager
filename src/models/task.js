08639cd0020328c121e12145bb150a508494094b
haluk <i.halukaksoy@gmail.com>
2021-07-11 01:43:49 +0300

    timestamps added to documents

--- src/models/task.js
+++ src/models/task.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const taskSchema = {
+const taskSchema = new mongoose.Schema({
     description: {
         type: String,
         required: true,
@@ -15,7 +15,9 @@ const taskSchema = {
         required: true,
         ref: 'User'
     }
-}
+},{
+    timestamps: true
+})
 
 const Task = mongoose.model('Task', taskSchema)
 
