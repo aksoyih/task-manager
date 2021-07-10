642c2ec06c386dd4712200721ed0cb47cdac2a34
haluk <i.halukaksoy@gmail.com>
2021-07-10 15:04:00 +0300

    relations between users and tasks are set up

--- src/models/task.js
+++ src/models/task.js
@@ -9,6 +9,11 @@ const taskSchema = {
     completed: {
         type: Boolean,
         default: false
+    },
+    owner:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
     }
 }
 
